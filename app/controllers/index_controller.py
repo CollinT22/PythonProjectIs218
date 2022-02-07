4fefe6ea2d4eb0adbcdbfb989418de5b8866179b
CollinT22 <61061400+CollinT22@users.noreply.github.com>
2022-02-07 18:21:52 -0500

    Update index_controller.py

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Collin Tully"
+        name = "Collin"
         return render_template('index.html', name=name)
